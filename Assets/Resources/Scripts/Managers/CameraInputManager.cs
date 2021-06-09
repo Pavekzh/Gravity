53e894edba1e8ceadaf31c815ea379c82a02517c
Denis Zhukovin <pavekzh@gmail.com>
2021-06-09 21:25:14 +0300

    Added camera controllers, Skybox.

---
+++ Assets/Resources/Scripts/Managers/CameraInputManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Resources.Library;
+
+public enum InputMode
+{
+    Moving,
+    Rotation
+}
+class CameraInputManager:Singleton<CameraInputManager>
+{
+    [SerializeField] private bool isControlLocked;
+    [SerializeField] private InputMode inputMode;
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Vector3 movingSpeed;
+
+    public InputMode InputMode { get => inputMode; set => inputMode = value; }
+    public bool IsControlLocked { get => isControlLocked; set => isControlLocked = value; }
+
+    private float DistanceBetweenTwoTouchPoints;
+
+    private void Update()
+    {
+        if(isControlLocked == false)
+        {
+            if(InputMode == InputMode.Moving)
+            {
+                CameraManager.Instance.Moving(ReadMovingInput());
+            }
+            else if (InputMode == InputMode.Rotation)
+            {
+                CameraManager.Instance.Rotation(ReadRotationInput());
+            }
+            CameraManager.Instance.Zoom(ReadZoomInput());
+        }
+    }
+    private float ReadZoomInput()
+    {
+        if (Input.touchCount >= 2)
+        {
+            float distance = 0;
+            distance = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)
+            {
+                DistanceBetweenTwoTouchPoints = distance;
+            }
+            else
+            {
+                float deltaDistance = (distance - DistanceBetweenTwoTouchPoints) * zoomSpeed;
+                DistanceBetweenTwoTouchPoints = distance;
+                return deltaDistance;
+            }
+        }
+        return 0;
+    }
+    private Vector3 ReadMovingInput()
+    {
+        if (Input.touchCount == 1/* && UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false*/)
+        {
+            Vector3 vector = new Vector3(Input.GetTouch(0).deltaPosition.x * movingSpeed.x, 0, -(Input.GetTouch(0).deltaPosition.y * movingSpeed.z));
+            return vector;
+        }
+        return Vector3.zero;
+    }
+    private Vector2 ReadRotationInput()
+    {
+        Vector2 OrbitAngle = new Vector2();
+        if (Input.touchCount == 1 /*&& UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false*/)
+        {
+            OrbitAngle.y = Input.GetTouch(0).deltaPosition.x * rotationSpeed;
+            OrbitAngle.x = -(Input.GetTouch(0).deltaPosition.y * rotationSpeed);
+            return OrbitAngle;
+        }
+        return Vector2.zero;
+    }
+    public void ChangeInputMode()
+    {
+        if (InputMode == InputMode.Moving)
+        {
+            this.InputMode = InputMode.Rotation;
+        }
+        else if (InputMode == InputMode.Rotation)
+        {
+            this.InputMode = InputMode.Moving;
+        }
+    }
+    public void ChangeInputMode(int Mode)
+    {
+        if (Mode == 0)
+            this.inputMode = InputMode.Moving;
+        else if (Mode == 1)
+            this.inputMode = InputMode.Rotation;
+    }
+}
+
---
+++ Assets/Resources/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Resources.Library;
+
+class CameraManager : Singleton<CameraManager>
+{
+    [Header("Limiters")]
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float minXAngle;
+    [SerializeField] private float maxXAngle;
+    [SerializeField] private float minRotationRadius;
+
+
+    [Header("Start data")]
+    [SerializeField] private float rotationRadius = 500;
+    [SerializeField] private Vector3 origin;
+    [SerializeField] private Vector2 orbitAngle;
+
+    public float RotationRadius
+    {
+        get
+        {
+            return rotationRadius;
+        }
+        set
+        {
+            if (value >= minRotationRadius)
+            {
+                if (value <= maxHeight || maxHeight == 0)
+                {
+                    rotationRadius = value;
+                }
+                else
+                {
+                    rotationRadius = maxHeight;
+                }
+            }
+            else
+            {
+                rotationRadius = minRotationRadius;
+            }
+        }
+    }
+    public Vector2 OrbitAngle
+    {
+        get { return orbitAngle; }
+        private set
+        {
+            if(value.x <= maxXAngle && value.x >= minXAngle)
+            {
+                orbitAngle = value;
+            }
+
+        }
+    }
+    public bool ControlLocked { get; set; } = false;
+
+    private const float DefaultRadius = 300;
+
+    private void Start()
+    {
+        ResetRotation();
+    }
+
+    public void Zoom(float DeltaDistance)
+    {
+        if(DeltaDistance != 0)
+        {
+            RotationRadius -= DeltaDistance * (rotationRadius / DefaultRadius);
+            ResetRotation();
+        }
+    }
+    public void Moving(Vector3 Vector)
+    {
+        if (Vector != Vector3.zero)
+        {
+            Vector = Vector * (rotationRadius / DefaultRadius);
+            LocalOriginTranslate(Vector);
+            ResetRotation();
+        }
+    }
+    public void Rotation(Vector2 OrbitDeltaAngle)
+    {
+        if (OrbitDeltaAngle != Vector2.zero)
+        {
+
+            OrbitAngle += OrbitDeltaAngle;
+            Debug.Log(OrbitAngle);
+
+            float r = RotationRadius * Mathf.Cos(Mathf.Deg2Rad * OrbitAngle.x);
+            float y = origin.y + (RotationRadius * Mathf.Sin(Mathf.Deg2Rad * OrbitAngle.x));
+
+            //rotation origin arround Y axis
+            Vector3 OriginY = new Vector3(origin.x, y, origin.z);
+
+            float x = r * Mathf.Sin(Mathf.Deg2Rad * orbitAngle.y);
+            float z = r * Mathf.Cos(Mathf.Deg2Rad * orbitAngle.y);
+
+            Vector3 newPosition = new Vector3(OriginY.x - x, OriginY.y, OriginY.z - z);
+            transform.position = newPosition;
+            transform.LookAt(origin);
+        }
+    }
+
+    private void LocalOriginTranslate(Vector3 vector)
+    {
+        this.origin.x -= vector.x * Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+        this.origin.z += vector.x * Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+
+        this.origin.z += vector.z * Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+        this.origin.x += vector.z * Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+
+        this.origin.y += vector.y;
+    }
+    private void TranslateY(float y)
+    {
+
+        float X = RotationRadius * Mathf.Cos(Mathf.Deg2Rad * OrbitAngle.x);
+        float Y = (RotationRadius * Mathf.Sin(Mathf.Deg2Rad * OrbitAngle.x)) + y;
+        float R = Mathf.Sqrt((Y * Y) + (X * X));
+        float Angle = Mathf.Acos((X * R) / (R * R)) * Mathf.Rad2Deg;
+
+        this.RotationRadius = R;
+        this.OrbitAngle = new Vector2(Angle,OrbitAngle.y);
+
+    }
+    private void ResetRotation()
+    {
+        float r = RotationRadius * Mathf.Cos(Mathf.Deg2Rad * OrbitAngle.x);
+        float y = origin.y + (RotationRadius * Mathf.Sin(Mathf.Deg2Rad * OrbitAngle.x));
+
+        //rotation origin arround Y axis
+        Vector3 OriginY = new Vector3(origin.x, y, origin.z);
+
+        float x = r * Mathf.Sin(Mathf.Deg2Rad * OrbitAngle.y);
+        float z = r * Mathf.Cos(Mathf.Deg2Rad * OrbitAngle.y);
+
+        Vector3 newPosition = new Vector3(OriginY.x - x, OriginY.y, OriginY.z - z);
+        transform.position = newPosition;
+        transform.LookAt(origin);
+    }
+
+}
+

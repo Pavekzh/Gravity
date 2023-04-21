b8aabead18e7485a68302c347c0543510e69df07
Denis Zhukovin <pavekzh@gmail.com>
2023-04-21 16:00:07 +0300

    Added & configured Zenject

---
+++ Assets/Infrastructure/BootstrapInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Infrastructure
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
---
+++ Assets/Infrastructure/MenuInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Infrastructure
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+
+    }
+}
